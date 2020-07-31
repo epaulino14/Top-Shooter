e3396b0969a7632bb5c3f7a1c8ee148acd71797c
epaulino14 <54982338+epaulino14@users.noreply.github.com>
2020-07-31 18:19:33 +0900

    Add files via upload

---
+++ BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollerSpeed = 0.5f;
+    Material myMaterial;
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, backgroundScrollerSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
